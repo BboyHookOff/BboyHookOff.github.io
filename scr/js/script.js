f4252fa9b62fb5191d07e71b409922227a0e4610
BboyHookOff <89813466+BboyHookOff@users.noreply.github.com>
2021-10-13 03:40:28 +0300

    Домашка к урокам 11-12
    1. Сделал отложенную анимацию для цифр в статистике. При их попадании в экран они плавно увеличиваются до заданного в коде значения. Если на сайте будет нажата какая либо ссылка, значение цифр снова опустится до нуля и, при наведении, сработает снова.
    2. Добавил карусель в отзывы и выровнял текст комментариев.
    3. Добавил модальное окно при нажатии на кнопку "скачать резюме".
    4. Оптимизировал прогрузку картинок в слайдере с кейсами (при их появлении подгружается картинка с лучшим качеством)
    5. добавил возможность просматривать картинки с помощью mafnific pop-up
    6. Подправил html и css
    Калькулятор и якорные ссылки к этому моменту уже были сделаны.

--- scr/js/script.js
+++ scr/js/script.js
@@ -1,18 +1,9 @@
 "use strict"
-
+// ---- рассчёт стоимости ------------------------------
 let def = "Выберите пункт...";
-let type = {
-  time: 0,
-  price: 0,
-}
-let design = {
-  time: 0,
-  price: 0,
-}
-let adapt = {
-  time: 0,
-  price: 0,
-}
+let type = { time: 0, price: 0, }
+let design = { time: 0, price: 0, }
+let adapt = { time: 0, price: 0, }
 let count = 0;
 function countType() {
   let indeX = document.getElementById("saitType").options.selectedIndex;
@@ -38,7 +29,7 @@ function countAdapt() {
   if(indeX == 2) { adapt.price = 2500; adapt.time = 0; }
   if(indeX == 3) { adapt.price = 5000; adapt.time = 0.5; }
 }
-setInterval(function () {
+function countUreWeb() {
   countType();
   countDesign();
   countAdapt();
@@ -46,7 +37,7 @@ setInterval(function () {
        document.getElementById("saitDesign").options.selectedIndex != 0 &&
        document.getElementById("saitAdapt").options.selectedIndex != 0 ){
     document.getElementById("worktime").innerHTML =
-      (type.time + design.time + adapt.time) + " недель";
+      (type.time + design.time + adapt.time) + " недели";
     document.getElementById("webcost").innerHTML =
       (type.price + design.price + adapt.price) + " рублей";
       if(count == 0){
@@ -54,4 +45,83 @@ setInterval(function () {
       count++;
     }
   }
-}, 100)
+}
+// ---- рассчёт стоимости ------------------------------
+
+// ---- анимация бегущих цифр в статистике -------------------
+let options = {threshold: [1]};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = $('.st_item'); //  $('.st_item');
+elements.each((i,el) => {
+  observer.observe(el);
+});
+
+let startItemOfStat = 0;
+let st1 = 100, st2 = 88, st3 = 3, st4 = 24; // будущие знацения 4-х статистик
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting){
+
+      setTimeout( function functionName() {
+          increaseStatistics("#st1",startItemOfStat,st1);
+          increaseStatistics("#st2",startItemOfStat,st2);
+          increaseStatistics("#st3",startItemOfStat,st3);
+          increaseStatistics("#st4",startItemOfStat,st4);
+          startItemOfStat = 10000; // чтобы повторно анимация не включалась на телефонах когда блоки идут сверху-вниз
+      },750)
+      // change.target.classList.add('op1');
+    }
+  });
+}
+
+function increaseStatistics(statisticClass, start, finish) {
+  let timer = 200/start;
+  if(start < finish){
+    if(start < finish-2){
+      setTimeout(function () {
+        start++;
+        $(statisticClass).text(start);
+        increaseStatistics(statisticClass, start, finish)
+      },timer);
+    }
+    else{
+      setTimeout(function () {
+        start++;
+        $(statisticClass).text(start);
+        increaseStatistics(statisticClass, start, finish)
+      },timer+200);
+    }
+  }
+}
+
+// после клика на ссылку анимация сбрасывается, чтобы она поворялась не единожды
+$('a').click(function () {
+  startItemOfStat = 0;
+  $('#st1').text(0);
+  $('#st2').text(0);
+  $('#st3').text(0);
+  $('#st4').text(0);
+});
+// ---- анимация бегущих цифр в статистике -------------------
+
+// ---- оптимизация картинок в слайдере ----------------------
+let options1 = {threshold: [0.5]};
+let observer1 = new IntersectionObserver(onEntry1, options1);
+let elements1 = $('.case-slide img'); // $('.case-slide img');$('.opt-picture');
+elements1.each((i,el) => {
+  observer1.observe(el);
+});
+
+function onEntry1(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting){
+      change.target.src = change.target.dataset.src;
+    }
+  });
+}
+// ---- оптимизация картинок в слайдере ----------------------
+
+// ----
+$(document).ready(function() {
+  $('.image-link').magnificPopup({type:'image'});
+});
